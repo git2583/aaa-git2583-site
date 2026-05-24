5c31c3b4decb828369b0f5e010a828f41de79ad8
git2583 <koreaasan@hanmail.net>
2026-05-24 21:35:26 +0900

    fix: basePath를 추가하여 서브디렉토리 에셋(CSS/JS) 경로 깨짐 현상 해결

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   // Next.js 정적 파일 단독 내보내기(SSG Export) 활성화
   output: 'export',
+  // 하브리드 배포 경로에 맞게 베이스 경로 매핑
+  basePath: '/dashboard',
   // 정적 빌드 시 Next.js 이미지 최적화 서버 미작동 오류를 예방하기 위한 옵션
   images: {
     unoptimized: true,
